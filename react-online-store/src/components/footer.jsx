23b63ad51ed292255716c57212ff12cdb21b0264
Ashton Ealum <ealumhome@gmail.com>
2025-11-12 17:53:46 -0600

    Assignments 1-3

---
+++ react-online-store/src/components/footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-bottom">
+          <p>© 2025 My Online Store. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
+export default Footer;
